d08451c2dc5c0b6dcb29c2582c9266575348c479
Kiran <kam535@cornell.edu>
2025-07-30 10:54:35 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Om6MRNBjcX65HjLjsBbFslzUvWwB05f20rAPUQwKSnE/edit?gid=0#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTNDUImq7ozCzqrLFFyMEFS-cTkON9uHHDIBXJ07KGAaKIgLlnaUfHOXFkmZCrW-zXS1901M1J0KJKo/pubhtml';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
